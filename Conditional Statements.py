9019bde13ccfb0c00b5d1e549ea6538f5feb712e
moulich6 <moulichimakurthi@gmail.com>
2026-01-28 17:05:05 -0600

    adding two files

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -1,4 +1,4 @@
-# Write a program that asks for a number and prints "Even" if the number is even, 
+'''# Write a program that asks for a number and prints "Even" if the number is even, 
 # otherwise prints "Odd".
 number = int(input('enter your number : '))
 if number %2 == 0:
@@ -43,4 +43,72 @@ if age>= 18:
     print('Adult')
 else:
     print('Minor')
+
+
+# Write a program that takes marks and prints the grade:
+
+#90 or above: A
+#80-89: B
+#70-79: C
+#60-69: D
+#Below 60: F
+
+marks = int(input('enter your makers : '))
+if marks >= 90:
+     print('A')
+elif marks >=80:
+     print('B')
+elif marks >=70:
+     print('c')
+elif marks >=60:
+     print('D')
+else:
+     print("F")
+
+
+#Question 2: Ticket Price
+#Ask for age. Calculate ticket price:
+#Under 5: Free
+#5-12: ₹100
+#13-17: ₹200
+#18-64: ₹300
+#65 or above: ₹150
+
+age = int(input('Plase provide your age : '))
+if age <= 5:
+     print('ticket price : Free')
+elif age <= 12:
+     print('ticket price : 100')
+elif age <= 17:
+     print('ticket price : 200')
+elif age <= 64:
+     print("ticket price : 300")
+else:
+     print('ticket price : 150')
+
+
+# Login System
+#Ask for username and password. Use nested conditionals:
+#If username is "admin", check if password is "admin123": "Admin access"
+#If username is "user", check if password is "user123": "User access"
+#Otherwise: "Access denied" '''
+
+user_name = input('enter your username : ')
+password = input('enter your password : ')
+if user_name == 'admin':
+    if password == 'admin123':
+        print('Admin access')
+    else:
+        print('wrong password \nAccess denied try again')
+elif user_name == 'user':
+    if password == 'user123':
+            print('User access')
+    else:
+         print('Access denied')
+else:
+        print('Access denied')
+
+     
+
+
 
---
+++ umesh_practice.py
@@ -0,0 +1,73 @@
+'''Question 1: Create Variables
+Create three variables:
+A variable called name that stores your name
+A variable called age that stores your age
+A variable called city that stores your city name'''
+
+name = "umesh"
+print(name)
+
+age = '25' # sting - '',""
+age = int(age)+1
+print(age)
+
+city = "guntur"
+print(city)
+
+
+'''Question 2: Calculate Total
+You bought 3 books at ₹150 each and 2 pens at ₹20 each. 
+Create variables for the quantities and prices, then calculate the total amount you spent.'''
+
+'''book = 150
+pen = 20
+quantitie_of_books =  3* book 
+quantitie_of_pens = 2* pen
+price = quantitie_of_books + quantitie_of_pens
+print(price)'''
+
+book_3 = 150*3
+pen_2 = 20*2
+total = book_3 + pen_2
+print(total)
+
+'''Question 3: Combine Text
+Create two variables:
+first_name with your first name
+last_name with your last name
+Then create a third variable full_name that combines both names with a space in between.'''
+
+first_name = 'umesh'
+last_name = 'dasari'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+
+'''Question 4: Update a Variable
+Start with a variable count that equals 10. Then add 5 to it. What is the new value of count?
+'''
+
+count = 10
+new_value = count + 5
+print(new_value)
+
+'''Question 6: Calculate Discount
+A shirt costs ₹500. There's a discount of ₹75. 
+Create variables for the original price and discount, 
+then calculate the final price after discount.'''
+
+original_price = 500
+discount_price = 75
+final_price = original_price - discount_price
+print(final_price)
+
+
+
+'''Question 7: Temperature Conversion
+The temperature in Celsius is 25. Create a variable celsius with this value. 
+Then create another variable fahrenheit that calculates the temperature in 
+Fahrenheit using the formula: fahrenheit = (celsius * 9/5) + 32'''
+
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+print(fahrenheit)
